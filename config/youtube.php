a446abd24e63b5bf67121bcc1f120590110506e5
hungluongduc@sohagame.vn <hungluongduc@sohagame.vn>
2018-05-02 15:16:27 +0700

    123

---
+++ config/youtube.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Laravel PHP Facade/Wrapper for the Youtube Data API v3
+|--------------------------------------------------------------------------
+|
+| Here is where you can set your key for Youtube API. In case you do not
+| have it, it can be acquired from: https://console.developers.google.com
+*/
+
+return [
+    'key' =>'613908046851-jabl73m3nikvv7480q83r3t6i73c3hdq.apps.googleusercontent.com'
+];
